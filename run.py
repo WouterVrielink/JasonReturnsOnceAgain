cc115e13ac77ef44f468f128231660845bd3508a
Haanstra <hella.haanstra@accenture.com>
2020-02-20 20:03:12 +0100

    scoring

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from load_and_dump import read_in, data_exists, dump_data, get_data, write_submission
+from score import get_score
 
 def sort_books(scores, book_ids):
     return sorted(book_ids, lambda book_id: scores[book_id])
@@ -18,18 +19,20 @@ def transform_format(data):
 
 if __name__ == "__main__":
     filename = 'a_example'
-    filename = 'b_read_on'
-    filename = 'c_incunabula'
-    filename = 'd_tough_choices'
-    filename = 'e_so_many_books'
-    filename = 'f_libraries_of_the_world'
+    # filename = 'b_read_on'
+    # filename = 'c_incunabula'
+    # filename = 'd_tough_choices'
+    # filename = 'e_so_many_books'
+    # filename = 'f_libraries_of_the_world'
     algorithm = 'hc'
 
     data = read_in(filename)
 
     output = transform_format(data)
+    score = get_score(output, data)
+    print(score)
 
-    write_submission(filename, output, mod='random')
+    # write_submission(filename, output, mod='random')
 
     # if not data_exists(filename, algorithm) or algorithm == 'init':
     #     # TODO DOE HIER INIT ALGORITME
